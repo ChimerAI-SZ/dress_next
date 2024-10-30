f50a194247319192b86298b9d4309056452e9554
oukyuushiba <oukyuushiba@gmail.com>
2024-10-30 16:43:47 +0800

    :beers: -

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -1,16 +1,14 @@
-"use client";
-
-import {
-  Container,
-} from "@chakra-ui/react";
+import { Container } from '@chakra-ui/react'
+import Header from './components/Header'
+import { DecorativeBox } from 'compositions/lib/decorative-box'
 
 function Page() {
-
   return (
-    <Container maxW="container.lg" p={3}>
-1
+    <Container maxW="container.lg">
+      <Header name="Like" addBtnvisible />
+      <DecorativeBox px="2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam consectetur, tortor in lacinia eleifend, dui nisl tristique nunc.</DecorativeBox>
     </Container>
-  );
+  )
 }
 
-export default Page;
+export default Page
