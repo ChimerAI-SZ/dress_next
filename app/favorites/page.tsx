e8b28d51bf5826f6daaf2640a3c98810617e91b4
oukyuushiba <oukyuushiba@gmail.com>
2024-10-30 19:09:02 +0800

    :beers: chakra 公用组件获取

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -1,12 +1,12 @@
 import { Container } from '@chakra-ui/react'
 import Header from './components/Header'
-import { DecorativeBox } from 'compositions/lib/decorative-box'
+import { Field } from '@components/ui/field'
 
 function Page() {
   return (
     <Container maxW="container.lg">
       <Header name="Like" addBtnvisible />
-      <DecorativeBox px="2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam consectetur, tortor in lacinia eleifend, dui nisl tristique nunc.</DecorativeBox>
+      <Field />
     </Container>
   )
 }
