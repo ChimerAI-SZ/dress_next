b30b64854120a3e11d4b05aad7a9831d07bd45ed
lz <13714733197@163.com>
2024-11-07 18:41:02 +0800

    修改

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,18 @@
-import Provider from './provider'
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+import Provider from "./provider";
+import { Suspense } from "react";
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html suppressHydrationWarning>
       <head />
       <body>
-        <Provider>{children}</Provider>
+        <Suspense>
+          <Provider>{children}</Provider>
+        </Suspense>
       </body>
     </html>
-  )
+  );
 }
