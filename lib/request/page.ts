a47ffa2da768655a3607ecf345c90ea78dd0a9d4
oukyuushiba <oukyuushiba@gmail.com>
2024-11-07 23:56:01 +0800

    :beers: alert封装

--- lib/request/page.ts
+++ lib/request/page.ts
@@ -1,9 +1,11 @@
 // 首页
-import axios from '../axios'
-
-
+import axios from "../axios"
 
 // 首页图库
-export const fetchHomePage = (params:object) => {
-    return axios.post('/api/image/list', params);
-};
+export const fetchHomePage = (params: object) => {
+  return axios.post("/api/image/list", params)
+}
+
+export const fetchCollectionList = (params: { user_id: string }) => {
+  return axios.post("/api/image/list", params)
+}
