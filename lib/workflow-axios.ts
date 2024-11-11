6483913ace34633eed8e7f68b2e7122d631fa360
lz <13714733197@163.com>
2024-11-11 14:42:34 +0800

    修改

--- lib/workflow-axios.ts
+++ lib/workflow-axios.ts
@@ -4,26 +4,31 @@ import axios, {
   CancelTokenSource,
   InternalAxiosRequestConfig,
   AxiosRequestHeaders,
-} from 'axios';
-import { exitLogin, storage } from '@utils/index';
+} from "axios";
+import { exitLogin, storage } from "@utils/index";
 
 interface ExtendedAxiosRequestConfig extends AxiosRequestConfig {
-  cancelToken?: CancelTokenSource['token'];
+  cancelToken?: CancelTokenSource["token"];
   headers: AxiosRequestHeaders;
   data?: any;
 }
 
 declare global {
   interface Window {
-      $message?: {
-          destroy: () => void;
-          error: (options: { content: string; duration: number; onClose: () => void }) => void;
-      };
+    $message?: {
+      destroy: () => void;
+      error: (options: {
+        content: string;
+        duration: number;
+        onClose: () => void;
+      }) => void;
+    };
   }
 }
-const instance = axios.create({ baseURL: "http://47.252.2.86:11118" || "/", timeout: 30000 });
-
-
+const instance = axios.create({
+  baseURL: "http://47.252.2.86:11118",
+  timeout: 30000,
+});
 
 // 用于存储 pending 的请求（处理多条相同请求）
 const pendingRequest = new Map<string, CancelTokenSource>();
@@ -31,16 +36,16 @@ const pendingRequest = new Map<string, CancelTokenSource>();
 // 生成 request 的唯一 key
 const generateRequestKey = (config: AxiosRequestConfig): string => {
   const { url, method, params, data } = config;
-  return [url, method, JSON.stringify(params), JSON.stringify(data)].join('&');
+  return [url, method, JSON.stringify(params), JSON.stringify(data)].join("&");
 };
 
 // 将重复请求添加到 pendingRequest 中
 const addPendingRequest = (config: ExtendedAxiosRequestConfig): void => {
   const key = generateRequestKey(config);
   if (!pendingRequest.has(key)) {
-      const source = axios.CancelToken.source();
-      config.cancelToken = source.token;
-      pendingRequest.set(key, source);
+    const source = axios.CancelToken.source();
+    config.cancelToken = source.token;
+    pendingRequest.set(key, source);
   }
 };
 
@@ -48,50 +53,51 @@ const addPendingRequest = (config: ExtendedAxiosRequestConfig): void => {
 const removePendingRequest = (config: AxiosRequestConfig): void => {
   const key = generateRequestKey(config);
   if (pendingRequest.has(key)) {
-      const source = pendingRequest.get(key);
-      if (source) source.cancel(key); // 取消之前发送的请求
-      pendingRequest.delete(key); // 请求对象中删除 requestKey
+    const source = pendingRequest.get(key);
+    if (source) source.cancel(key); // 取消之前发送的请求
+    pendingRequest.delete(key); // 请求对象中删除 requestKey
   }
 };
 
 instance.interceptors.request.use(
   (config) => {
-      const extendedConfig = config as ExtendedAxiosRequestConfig;
-      removePendingRequest(extendedConfig); // 在一个 ajax 发送前执行一下取消操作
-      addPendingRequest(extendedConfig);
+    const extendedConfig = config as ExtendedAxiosRequestConfig;
+    removePendingRequest(extendedConfig); // 在一个 ajax 发送前执行一下取消操作
+    addPendingRequest(extendedConfig);
 
-      const TOKEN = storage.get("token");
-      if (TOKEN) {
-          extendedConfig.headers = extendedConfig.headers || {};
-          extendedConfig.headers['Content-Type'] = extendedConfig.headers['Content-Type'] || 'application/json';
-          extendedConfig.headers.Authorization = TOKEN;
-      }
+    const TOKEN = storage.get("token");
+    if (TOKEN) {
+      extendedConfig.headers = extendedConfig.headers || {};
+      extendedConfig.headers["Content-Type"] =
+        extendedConfig.headers["Content-Type"] || "application/json";
+      extendedConfig.headers.Authorization = TOKEN;
+    }
 
-      return extendedConfig;
+    return extendedConfig;
   },
   (error) => {
-      return Promise.reject(error);
+    return Promise.reject(error);
   }
 );
 
 instance.interceptors.response.use(
   (response: AxiosResponse) => {
-      // 在一个 ajax 响应后再执行一下取消操作，把已经完成的请求从 pending 中移除
-      removePendingRequest(response.config);
-      if (response.status === 401) {
-          // exitLogin();
-      }
-      return response.data;
+    // 在一个 ajax 响应后再执行一下取消操作，把已经完成的请求从 pending 中移除
+    removePendingRequest(response.config);
+    if (response.status === 401) {
+      // exitLogin();
+    }
+    return response.data;
   },
   (error) => {
-      const { status } = error?.response || {};
-      if (status === 500) {
-          window.$message?.destroy();
-          console.error(status);
-      } else if (status === 401) {
-          // exitLogin();
-      }
-      return Promise.reject(error);
+    const { status } = error?.response || {};
+    if (status === 500) {
+      window.$message?.destroy();
+      console.error(status);
+    } else if (status === 401) {
+      // exitLogin();
+    }
+    return Promise.reject(error);
   }
 );
 
