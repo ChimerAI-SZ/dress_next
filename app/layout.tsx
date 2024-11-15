086e0842ec3f98cf749dcce6c74e0d4fe8204683
lz <13714733197@163.com>
2024-11-15 17:56:34 +0800

    添加页面关键词

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,35 @@
-"use client";
-import Provider from "./provider";
-import { Suspense } from "react";
-import "./global.css";
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+"use client"
+import Head from "./head"
+import Provider from "./provider"
+import { Suspense } from "react"
+import "./global.css"
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html suppressHydrationWarning>
-      <head />
+      <head>
+        <meta
+          name="description"
+          content="Introducing CREAMODA, an unprecedented clothing generative engine. At CREAMODA, we offer an intelligent fashion platform designed to bring your creative ambitions to life. With our state-of-the-art generative clothing engine, CREAMODA transforms your vision into top-selling fashion, seamlessly and efficiently. Whether you’re shaping trends or creating unique designs, CREAMODA empowers you to bring your ideas to market with ease."
+        />
+        <meta
+          name="keywords"
+          content="AI fashion creation
+Generative clothing engine
+AI-driven fashion platform
+Fashion design automation
+Seamless fashion design
+Top-selling fashion designs
+AI fashion technology
+Intelligent fashion design
+Creative fashion platform
+Automated garment creation"
+        />
+        <link rel="icon" href="/icon.png" />
+      </head>
       <body
         style={{
-          overflow: "auto",
+          overflow: "auto"
         }}
       >
         <Suspense>
@@ -20,5 +37,5 @@ export default function RootLayout({
         </Suspense>
       </body>
     </html>
-  );
+  )
 }
