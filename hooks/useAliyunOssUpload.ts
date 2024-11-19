884c1fe83cdef0a8f33e7ce5416c5288a16ac302
lz <13714733197@163.com>
2024-11-19 11:50:16 +0800

    格式转换

--- hooks/useAliyunOssUpload.ts
+++ hooks/useAliyunOssUpload.ts
@@ -45,7 +45,7 @@ const useAliyunOssUpload = (): UseAliyunOssUploadReturn => {
       })
 
       // 构建文件的URL
-      const uploadedUrl = `https://${process.env.NEXT_PUBLIC_OSS_BUCKET}.${process.env.NEXT_PUBLIC_OSS_REGION}.aliyuncs.com/${result.name}/format,jpg`
+      const uploadedUrl = `https://${process.env.NEXT_PUBLIC_OSS_BUCKET}.${process.env.NEXT_PUBLIC_OSS_REGION}.aliyuncs.com/${result.name}?x-oss-process=image/format,jpg`
 
       setUploadState({
         isUploading: false,
