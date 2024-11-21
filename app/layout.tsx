8d58ff99acf0dcdf4a86bdab06a4e6de26f68bf7
oukyuushiba <oukyuushiba@gmail.com>
2024-11-22 00:39:51 +0800

    -

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,18 @@ export const viewport: Viewport = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html suppressHydrationWarning>
-      <head></head>
+      <head>
+        <title>CREAMODA</title>
+        <link rel="icon" href="/icon.png" />
+        <meta
+          name="description"
+          content="Introducing CREAMODA, an unprecedented clothing generative engine. At CREAMODA, we offer an intelligent fashion platform designed to bring your creative ambitions to life. With our state-of-the-art generative clothing engine, CREAMODA transforms your vision into top-selling fashion, seamlessly and efficiently. Whether you’re shaping trends or creating unique designs, CREAMODA empowers you to bring your ideas to market with ease."
+        />
+        <meta
+          name="keywords"
+          content="AI fashion creation Generative clothing engine AI-driven fashion platform Fashion design automation Seamless fashion design Top-selling fashion designs AI fashion technology Intelligent fashion design Creative fashion platform Automated garment creation"
+        />
+      </head>
       <body
         style={{
           overflow: "auto"
@@ -23,16 +34,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       >
         <Suspense>
           <Helmet>
-            <title>CREAMODA</title>
-            <link rel="icon" href="/icon.png" />
-            <meta
-              name="description"
-              content="Introducing CREAMODA, an unprecedented clothing generative engine. At CREAMODA, we offer an intelligent fashion platform designed to bring your creative ambitions to life. With our state-of-the-art generative clothing engine, CREAMODA transforms your vision into top-selling fashion, seamlessly and efficiently. Whether you’re shaping trends or creating unique designs, CREAMODA empowers you to bring your ideas to market with ease."
-            />
-            <meta
-              name="keywords"
-              content="AI fashion creation Generative clothing engine AI-driven fashion platform Fashion design automation Seamless fashion design Top-selling fashion designs AI fashion technology Intelligent fashion design Creative fashion platform Automated garment creation"
-            />
             {/* 解决浏览器在聚焦输入框时缩放的问题 begins  */}
             {/* 解决方式是禁止浏览器缩放（同时会仅用用户的缩放） */}
             <meta name="apple-mobile-web-app-capable" content="yes" />
